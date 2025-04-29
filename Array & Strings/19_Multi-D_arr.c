b4be86ef1b30f5840f8423cfd273eb089c60cc5c
uvpatel <uvpatel7271@gmail.com>
2025-04-29 20:04:14 +0530

    add Mutidimensional array

---
+++ Array & Strings/19_Multi-D_arr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    
+    for 
+    For multidimensional array:
+    A[3][4][2][3] →
+    No of elements present in the array :
+    (product of all subscript) 3 * 4 * 2 *3 = 72
+    No of column = 3 (last subscript)
+    No of rows = (product of subscript except last subscript) 3 * 4 * 2 = 24 
+*/
+    return 0;
+}
