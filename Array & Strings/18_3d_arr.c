874c538374547cf8d44ad9cc041cac4aae26593a
uvpatel <uvpatel7271@gmail.com>
2025-04-29 20:02:14 +0530

    Demonstrated one example of 3-D array

--- Array & Strings/18_3d_arr.c
+++ Array & Strings/18_3d_arr.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(){
+    int arr[2][3][2] = {
+        {{1,1},{2,3},{4,5}},
+        {{6,7},{8,9},{10,11}}
+    };
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            for (int k = 0; k < 2 ; k++)
+            {
+                printf("arr[%i][%i][%i] = %d",i,j,k,arr[i][j][k]);
+            }
+            printf("\n");
+            
+        }
+        printf("\n");
+    }
     
     return 0;
 }
