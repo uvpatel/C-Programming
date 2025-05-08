0ba3d2a10bc868f348c62bf6e3a9a4eba7e0c4d4
uvpatel <uvpatel7271@gmail.com>
2025-05-08 22:02:32 +0530

    learn how to reverse a number

--- C-Practice-set/Practical set - 5/pro2.c
+++ C-Practice-set/Practical set - 5/pro2.c
@@ -2,5 +2,18 @@
 
 int main(){
     
+    int n,rem,rev_n = 0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    do
+    {
+
+        int  rem = n % 10;
+        n /= 10;
+        rev_n = rev_n*10 + rem;
+
+    } while (n != 0);
+    printf("The reverse number is %d",rev_n);
     return 0;
 }
