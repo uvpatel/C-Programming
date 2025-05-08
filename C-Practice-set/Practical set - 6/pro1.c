8ad6a4e2e220eae022f779fd2d8ee0725551420d
uvpatel <uvpatel7271@gmail.com>
2025-05-08 22:16:40 +0530

    solved the practical set -6 problems

--- C-Practice-set/Practical set - 6/pro1.c
+++ C-Practice-set/Practical set - 6/pro1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(){
-    
+    int iterations;
+    for (int i = 0; i < iterations; i++)
+    {
+        
+        
+        float num,cube;
+        printf("Enter the number plate: ");
+        scanf("%f", &num);
+        cube = pow(num,3);
+        
+        printf("The cube of the number is: %f\n", cube);
+    }
+
     return 0;
 }
