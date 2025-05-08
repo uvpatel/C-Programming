234883906aae06f9eb52fe03e1f274dbe66f1706
uvpatel <uvpatel7271@gmail.com>
2025-05-08 22:13:30 +0530

    Pelidrome number

--- C-Practice-set/Practical set - 5/pro4.c
+++ C-Practice-set/Practical set - 5/pro4.c
@@ -2,5 +2,27 @@
 
 int main(){
     
+    int n,rem,rev_n = 0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+
+    while (n != 0)
+    {
+        int  rem = n % 10;
+        n /= 10;
+        rev_n = rev_n*10 + rem;
+        
+    }
+    if (rev_n == n)
+    {
+        printf("The number is palindrome.\n");
+    }
+    else
+    {
+        printf("The number is not palindrome.\n");
+    }
+    
+    
     return 0;
 }
