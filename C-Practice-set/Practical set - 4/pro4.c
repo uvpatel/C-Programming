0501617fb29854b820c074d6f8b5e8ec195033db
uvpatel <uvpatel7271@gmail.com>
2025-05-08 21:46:29 +0530

    added file

--- C-Practice-set/Practical set - 4/pro4.c
+++ C-Practice-set/Practical set - 4/pro4.c
@@ -3,5 +3,74 @@
 int main(){
     /*Menu: 
     panipuri , Sandwich, Vadapav,Pavbhaji,Dosa*/
+
+    int choice;
+    float bill;
+    printf("Enter Your Choice Food( panipuri - 1  , Sandwich - 2, Vadapav - 3 ,Pavbhaji - 4,Dosa - 5): ");
+    scanf("%d",&choice);
+    
+    
+    switch (choice)
+    {
+        case 1:
+        printf("Your Choice is Panipuri. Great Choice.");
+        printf("The Price of the Pani-Puri 40 Ruppes.");
+        bill += 40;
+        // int order;
+        // printf("Do You like to order anything else. From Manu.");
+        // scanf("%d",&order);
+        // printf("Enter Your Choice Food( panipuri - 1  , Sandwich - 2, Vadapav - 3 ,Pavbhaji - 4,Dosa - 5): ");
+        // scanf("%d",&choice);
+        // bill += 
+
+        // if (order == 0)
+        // {
+        //     printf("What whould you like to have: ");
+
+        // }
+        // else{
+        //     printf("Thank you sir");
+        // }
+        // break;
+
+
+
+        
+        case 2:
+        printf("Your Choice is Sandwich. Great Choice.");
+        printf("The Price of the Sandwich 80 Ruppes.");
+        bill += 80;
+        break;
+
+        case 3:
+        printf("Your Choice is VadaPav. Great Choice.");
+        printf("The Price of the Sandwich 20 Ruppes.");
+        bill += 20;
+        break;
+
+        case 4:
+        printf("Your Choice is Pav-Bhaji. Great Choice.");
+        printf("The Price of the Pav-Bhaji 100 Ruppes.");
+        bill += 100;
+        break;
+
+
+        case 5:
+        printf("Your Choice is Dosa. Great Choice.");
+        printf("The Price of the Dosa 120 Ruppes.");
+        bill += 120;
+        break;
+
+        
+        
+     default:
+        printf("Given Ordered Item is not available.");
+        break;
+
+        
+    }
+
+    printf("The Final Price is %f",bill);
+
     return 0;
 }
